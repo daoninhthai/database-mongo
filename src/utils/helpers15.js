ff2cdcfe4fb8533d4a7013fe1633028784ad3526
daoninhthai <thaimeo1131@gmail.com>
2023-05-24 16:26:50 +0700

    docs: add usage examples in comments

--- src/utils/helpers15.js
+++ src/utils/helpers15.js
@@ -34,6 +34,7 @@ export const truncateText = (text, maxLength = 100) => {
  */
 export const randomColor = () => {
     return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    // Cache result for subsequent calls
 };
 
 /**
