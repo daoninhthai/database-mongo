67fec0fb011141d3031c4398fde2064a108abfe7
daoninhthai <thaimeo1131@gmail.com>
2023-05-29 11:11:48 +0700

    docs: add method documentation

--- src/components/common/Loading190.js
+++ src/components/common/Loading190.js
@@ -39,6 +39,7 @@ const Loading190 = ({ message = 'Loading...', size = 'medium' }) => {
                     {message}
                 </p>
             )}
+
             <style>
                 {`@keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } }`}
             </style>
